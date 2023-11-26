5c60b1ef01c10acdab1c23c5196313b260fe43f2
krysthopher@gmail.com <krysthopher@gmail.com>
2023-11-26 16:36:41 -0600

    added all the content, also created the reset button

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,3 @@
-
-
-
-
-
 const workStages = document.getElementById('stages');
 const stage1Content = document.getElementById('stage1');
 const stage2Content = document.getElementById('stage2');
@@ -37,21 +32,43 @@ workStages.addEventListener('change', function () {
   } else if (selectedStage === 'stage2') {
     stage2Content.style.display = 'block';
   }else if (selectedStage === 'stage3') {
-    stage2Content.style.display = 'block';
+    stage3Content.style.display = 'block';
   }else if (selectedStage === 'stage4') {
-    stage2Content.style.display = 'block';
+    stage4Content.style.display = 'block';
   }else if (selectedStage === 'stage5') {
-    stage2Content.style.display = 'block';
+    stage5Content.style.display = 'block';
   }else if (selectedStage === 'stage6') {
-    stage2Content.style.display = 'block';
+    stage6Content.style.display = 'block';
   }else if (selectedStage === 'stage7') {
-    stage2Content.style.display = 'block';
+    stage7Content.style.display = 'block';
   }else if (selectedStage === 'stage8') {
-    stage2Content.style.display = 'block';
+    stage8Content.style.display = 'block';
   }else if (selectedStage === 'stage9') {
-    stage2Content.style.display = 'block';
+    stage9Content.style.display = 'block';
   }else if (selectedStage === 'stage10') {
-    stage2Content.style.display = 'block';
+    stage10Content.style.display = 'block';
   }
   // Add more conditions for additional content sections
+});
+
+//reset button scrip 
+
+const resetButton = document.getElementById('reset-button');
+
+resetButton.addEventListener('click', function () {
+  // Reset the dropdown to its initial state (first option)
+  workStages.selectedIndex = 0;
+
+  // Hide all content sections
+  stage1Content.style.display = 'none';
+  stage2Content.style.display = 'none';
+  stage3Content.style.display = 'none';
+  stage4Content.style.display = 'none';
+  stage5Content.style.display = 'none';
+  stage6Content.style.display = 'none';
+  stage7Content.style.display = 'none';
+  stage8Content.style.display = 'none';
+  stage9Content.style.display = 'none';
+  stage10Content.style.display = 'none';
+  // Add more content sections if needed
 });
